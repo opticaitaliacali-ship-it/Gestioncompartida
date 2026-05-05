08cb1cdd2f3e76ae87b51980808131760a950707
opticaitaliacali-ship-it <opticaitaliacali@gmail.com>
2026-05-04 19:13:49 -0500

    cambiofer

--- index.php
+++ index.php
@@ -119,7 +119,7 @@
 <body>
 
 <header>
-  <h1>📘 Historia del Cálculo Integral..</h1>
+  <h1>📘 HISTORIA DEL CALCULO INTEGRAL..</h1>
   <p>Origen y evolución de una de las ramas más importantes de las matemáticas</p>
 </header>
 
